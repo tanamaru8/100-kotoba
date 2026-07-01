cd02a39ba7d1916d1cce347a9d8be7a08e88d5f2
tanamaru8 <kousukecabrera@gmail.com>
2026-07-02 02:25:26 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("サイト起動");
+// ==============================
+// ハンバーガーメニュー
+// ==============================
+
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+
+// メニュー開閉
+hamburger.addEventListener("click", () => {
+    menu.classList.toggle("open");
+});
+
+// メニューをクリックしたら閉じる
+const links = menu.querySelectorAll("a");
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        menu.classList.remove("open");
+    });
+});
